1f3a2d0ea62293255d298ac24be60014d44e86f2
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-22 22:04:33 +0100

    new: database module

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const { logger } = require('../src/utils/logger')
+
+
+module.exports = ( URI ) => {
+    mongoose.set('strictQuery', false)
+    mongoose
+        .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            logger.info('Connection to MongoDB successful')
+        })
+        .catch((err) => {
+            logger.error('Connection to MongoDB failed', err.message)
+        })
+} 
