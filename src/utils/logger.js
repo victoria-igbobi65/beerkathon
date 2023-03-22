cdba33636602b0267d8c00ee7c85325f71aac342
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-22 22:05:53 +0100

    new: logger file

---
+++ src/utils/logger.js
@@ -0,0 +1,19 @@
+const winston = require('winston')
+const CONFIG = require('../../config/env')
+
+const logger =
+    CONFIG.NODE_ENV === 'test'
+        ? winston.createLogger({ silent: true })
+        : winston.createLogger({
+              transports: [
+                  new winston.transports.Console({
+                      level: 'info',
+                      format: winston.format.combine(
+                          winston.format.colorize(),
+                          winston.format.simple()
+                      ),
+                  }),
+              ],
+          })
+
+module.exports = { logger }
