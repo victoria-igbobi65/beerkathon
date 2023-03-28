2ce61ba125bb90bdd89bafbe1cd6c793513db997
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-28 20:15:55 +0100

    improve: added env variables

--- config/env.js
+++ config/env.js
@@ -2,8 +2,12 @@ require('dotenv').config()
 
 const DBURL = process.env.NODE_ENV === 'test'? process.env.TESTDB_URL: process.env.DB_URL;
 const PORT = process.env.PORT || 3000;
+const EMAIL_FROM = process.env.FROM;
+const GMAIL_PASS = process.env.PASS;
 
 module.exports={
     DBURL,
-    PORT
+    PORT,
+    EMAIL_FROM,
+    GMAIL_PASS
 }
