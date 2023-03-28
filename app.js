3f29affdf28caa5722e4bf1ded06ab8d27b150be
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-28 07:06:40 +0100

    improve: handled unkown endpoint and implemented global error handler

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const express = require('express')
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const globalErrorHandler = require('./src/errors/errorhandler')
+const unknownEndpoint = require('./src/middlewares/unknownEndpoint')
 const CONFIG = require('./config/env')
 require('./config/db')( CONFIG.DBURL )
 
 const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'))
+app.use(cookieParser());
+app.use(cors()); 
+app.use(helmet());
+
+
+
+
+
+app.use("*", unknownEndpoint )
+app.use( globalErrorHandler )
+
 
 module.exports={ app }
