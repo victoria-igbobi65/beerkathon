c249fcf0628a914f50499f855c6d7e74f541ff58
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-29 20:49:41 +0100

    improve: added helper functions

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,7 +1,27 @@
 const {v4: uuid4} = require('uuid')
+const jwt = require("jsonwebtoken");
+const { promisify } = require('util')
+const CONFIG = require('../../config/env')
 
 const uniqueId = () => {
     return uuid4()
 }
 
-module.exports={ uniqueId }
+const signToken = (id) => {
+    return jwt.sign({ id }, CONFIG.SECRET);
+};
+
+const setCookies = (res, tokenName, token) => {
+    return res.cookie(tokenName, token, {
+        httpOnly: true,
+        secure: CONFIG.APP_ENV,
+        maxAge: CONFIG.MAXAGE
+    });
+};
+
+const decodeToken = async (token) => {
+    return promisify(jwt.verify)(token, CONFIG.SECRET);
+};
+
+
+module.exports={ uniqueId, signToken, setCookies, decodeToken }
