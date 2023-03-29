dc018418ee2cfc1c86235888e0ddea48d02e2b7b
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-29 20:50:43 +0100

    fix: improved the email from

--- src/utils/email/sendMail.js
+++ src/utils/email/sendMail.js
@@ -6,7 +6,7 @@ const welcomeMail = async ( email, password ) => {
     const { subject, message } = emailTemplate( email, password )
 
     return sendEmail({
-        from: "Meal Tracker <noreply@gmail.com>",
+        from: "noreply@gmail.com",
         to: email,
         subject: subject,
         html: message,
