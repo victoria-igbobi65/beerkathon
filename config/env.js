5f4063159b25777405bd0408abd495cd829bc404
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-29 20:46:20 +0100

    improve: added config variables

--- config/env.js
+++ config/env.js
@@ -4,10 +4,16 @@ const DBURL = process.env.NODE_ENV === 'test'? process.env.TESTDB_URL: process.e
 const PORT = process.env.PORT || 3000;
 const EMAIL_FROM = process.env.FROM;
 const GMAIL_PASS = process.env.PASS;
+const APP_ENV = "production";
+const SECRET = process.env.SECRET_KEY;
+const MAXAGE = 60 * 60 * 1000;
 
 module.exports={
     DBURL,
     PORT,
     EMAIL_FROM,
-    GMAIL_PASS
+    GMAIL_PASS,
+    APP_ENV,
+    SECRET,
+    MAXAGE
 }
