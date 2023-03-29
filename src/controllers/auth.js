8e6c3e134ba5b0631a7d7cc63891eb3717ead851
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-29 20:46:54 +0100

    improve: completed signup and login controllers

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,22 +1,42 @@
 const { StatusCodes } = require('http-status-codes')
 const { AppError } = require("../errors/AppError");
 const { getUser, addUser } = require("../services/auth");
-const { uniqueId } = require('../utils/helper');
+const { uniqueId, signToken, setCookies } = require('../utils/helper');
 const catchAsync = require('../errors/catchAsync');
 const { welcomeMail } = require('../utils/email/sendMail');
+const transaction = require('../utils/mongooseTrans')
 
 exports.signup = catchAsync( async( req, res) => {
 
     const { email } = req.body;
     const employeePassword = uniqueId()
-
-    await addUser({ email: email, password: employeePassword })
-    await welcomeMail( email, employeePassword )
+    
+    await transaction( async( session ) => {
+        await addUser({ email: email, password: employeePassword });
+        await welcomeMail(email, employeePassword);
+    })
     
     res.status( StatusCodes.OK ).json({
         status: true,
         msg: "User registration successful!"
-    })
-    
+    })   
 
+})
+
+exports.login = catchAsync( async( req, res) => {
+
+    const { email, password } = req.body;
+    const user = await getUser({ email: email })
+
+    if ( !user || !( await user.correctPassword(password, user.password ))){
+        throw new AppError( 'email or password incorrect!', StatusCodes.BAD_REQUEST )
+    }
+
+    const token = signToken( user._id )
+    setCookies( res, 'refresh_token', token )
+
+    res.status( StatusCodes.OK ).json({
+        status: true,
+        msg: "Login successful!"
+    })
 })
