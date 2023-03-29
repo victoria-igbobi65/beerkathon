e02aa1147e96664e1e191e9f665777ad251a881e
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-29 20:49:17 +0100

    improve: mounted login routes for admin and plain users

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,13 +1,21 @@
 const express = require('express')
 
 const authController = require('../controllers/auth')
-const { signupDto } = require('../validators/auth');
+const { signupDto, loginDto } = require('../validators/auth');
 const { userExists } = require('../middlewares/verify');
+const protect = require('../middlewares/protect')
 const authRouter = express.Router();
 
 authRouter
     .route('/signup')
-    .post( signupDto, userExists, authController.signup )
+    .post( protect, signupDto, userExists, authController.signup )
 
+authRouter
+    .route('/login')
+    .post( loginDto, authController.login )
+
+authRouter
+    .route('/admin/login')
+    .post( loginDto, authController.login )
 
 module.exports={ authRouter }
