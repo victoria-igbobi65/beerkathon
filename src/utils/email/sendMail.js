35a0328d54f3d98e6feeb54ed1ce363cadda7154
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-30 08:45:47 +0100

    fix: from error

--- src/utils/email/sendMail.js
+++ src/utils/email/sendMail.js
@@ -6,7 +6,7 @@ const welcomeMail = async ( email, password ) => {
     const { subject, message } = emailTemplate( email, password )
 
     return sendEmail({
-        from: "noreply@gmail.com",
+        from: `Meal Tracker<noreply@gmail.com>`,
         to: email,
         subject: subject,
         html: message,
