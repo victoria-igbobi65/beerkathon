029e5322f8b942e3b88b82f791652f1e38153b7e
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-03-30 19:51:55 +0100

    new: middleware to check if user is eligible for a new ticket

---
+++ src/middlewares/eligibile.js
@@ -0,0 +1,18 @@
+const { StatusCodes } = require('http-status-codes')
+const catchAsync = require("../errors/catchAsync");
+const { AppError } = require('../errors/AppError');
+const { getUser } = require('../services/auth');
+
+
+const eligible = catchAsync( async( req, res, next ) => {
+    
+    const userId = req.user;
+    const user = await getUser({ _id: userId })
+
+    if ( !user.ticketAvailable ){
+        throw new AppError("Check back tomorrow for tickets!", StatusCodes.BAD_REQUEST )
+    }
+    next();
+})
+
+module.exports={ eligible }
