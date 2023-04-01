f788eb608f3fff6b979971f8e2fb777c654e5ac9
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-04-01 07:43:48 +0100

    new: delete a user routing

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,10 +2,15 @@ const express = require('express')
 const userController = require('../controllers/users')
 
 const protect = require('../middlewares/protect')
+const validateId = require('../middlewares/validate')
 const userRouter = express.Router()
 
 userRouter
     .route('/')
     .get( protect, userController.getEmployees )
+
+userRouter
+    .route('/:id')
+    .delete( protect, validateId, userController.deleteUser )
 
 module.exports={ userRouter }
