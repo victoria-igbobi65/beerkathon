52179dc5c522de2b41707588fef3981603a7d7c7
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-04-01 23:21:41 +0100

    new: mounted routes

--- src/routes/ticket.js
+++ src/routes/ticket.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const ticketController = require('../controllers/ticket')
 const authenticateUser = require('../middlewares/authenticate');
+const validateTicketId = require('../middlewares/validateTicketId')
+const protect = require('../middlewares/protect')
 const { eligible } = require('../middlewares/eligibile');
 
 
@@ -8,7 +10,13 @@ const ticketRouter = express.Router();
 
 ticketRouter
     .route('/')
-    .get( authenticateUser, eligible, ticketController.ticket )
+    .post( authenticateUser, protect(["user"]), eligible, ticketController.ticket )
+    .get( authenticateUser, protect(['admin', 'vendor']), ticketController.allOrders )
+
+
+ticketRouter
+    .route('/:id')
+    .patch( authenticateUser, protect(["vendor"]), validateTicketId, ticketController.updateTicket )
 
 
 module.exports={ ticketRouter }
