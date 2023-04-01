187ae9fdaaeaf55fbf666cf2c90eda698cadd13e
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-04-01 23:23:33 +0100

    new: added orders services

--- src/services/ticket.js
+++ src/services/ticket.js
@@ -5,4 +5,16 @@ const saveTicket = async( object ) => {
     return ticketModel.create( object )
 }
 
-module.exports={ saveTicket }
+const updateTicketStatus = async( id, status ) => {
+    return ticketModel.findOneAndUpdate(id, { $set: { status }}, { new: true, runValidators: true })
+}
+
+const getTicket = async( object ) => {
+    return ticketModel.findOne( object )
+}
+
+const getTickets = async() => {
+    return ticketModel.find({})
+}
+
+module.exports={ saveTicket, updateTicketStatus, getTicket, getTickets }
