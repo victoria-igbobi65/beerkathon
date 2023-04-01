2205fef2f359025520cf18cefc82dabe1e5aaf5d
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-04-01 23:18:48 +0100

    new: validate orders ID

---
+++ src/middlewares/validateTicketId.js
@@ -0,0 +1,17 @@
+const { StatusCodes } = require("http-status-codes");
+const catchAsync = require("../errors/catchAsync");
+const { getTicket } = require("../services/ticket");
+const { AppError } = require("../errors/AppError");
+
+
+module.exports = catchAsync( async( req, res, next ) => {
+
+    const id = req.params.id;
+    const found = await getTicket({ _id: id })
+
+    if( !found ){
+        throw new AppError('Meal doesn\'t exist!', StatusCodes.NOT_FOUND )
+    }
+
+    next();
+})
