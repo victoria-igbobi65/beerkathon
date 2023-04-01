dcf1c381b1d6d49ce92705d65dbfd22322007104
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-04-01 23:17:45 +0100

    improve: allows only specified roles to access API endpoint

--- src/middlewares/protect.js
+++ src/middlewares/protect.js
@@ -5,22 +5,14 @@ const { AppError } = require('../errors/AppError');
 const { decodeToken } = require('../utils/helper');
 
 
-module.exports = catchAsync( async( req, res, next) => {
-    const token = req.cookies.refresh_token;
-
-    if (!token) {
-        throw new AppError("You are not logged in!", StatusCodes.FORBIDDEN );
-    }
-    const userId = (await decodeToken(token)).id;
-
-    if( !userId){
-        throw new AppError("Invalid token!", StatusCodes.FORBIDDEN )
-    }
-    const user = await getUser({ _id: userId })
-
-    if ( user && !user.isAdmin){
-        throw new AppError('Unauthorized action!', StatusCodes.UNAUTHORIZED )
-    }
-
-    next()
-})
+module.exports = ( roles ) => {
+    return catchAsync(async (req, res, next) => {
+        
+        const user = req.user;
+        if( !roles.includes( user.user_type )){
+            throw new AppError('Unauthorized action!', StatusCodes.UNAUTHORIZED )
+        }
+        
+        next();
+    });
+}
