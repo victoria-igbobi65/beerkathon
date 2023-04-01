2afc6971913685cc3823631b78646587c864cde9
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-04-01 07:18:06 +0100

    new: meal details validator

---
+++ src/validators/meal.js
@@ -0,0 +1,35 @@
+const joi = require("@hapi/joi");
+
+const validateBody = (schema) => {
+    return (req, res, next) => {
+        const result = schema.validate(req.body);
+
+        if (result.error) {
+            return res.status(400).json({ message: result.error.message });
+        }
+
+        next();
+    };
+};
+
+
+/* Login validator */
+const mealSchema = joi.object().keys({
+    meal : joi
+        .string()
+        .trim()
+        .required()
+        .error(new Error("Meal name is required!")),
+    price: joi
+        .number()
+        .min(1)
+        .required()
+        .error(new Error("Meal price is required!")),
+});
+
+
+const mealDto = validateBody(mealSchema);
+
+module.exports = {
+    mealDto
+};
