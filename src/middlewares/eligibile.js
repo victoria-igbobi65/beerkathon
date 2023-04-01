f955ffa94ee25ce3be74cffb5b12c57c4f9baad5
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-04-01 07:14:32 +0100

    fix: only only employees to be able to request meal and get tickets

--- src/middlewares/eligibile.js
+++ src/middlewares/eligibile.js
@@ -9,6 +9,10 @@ const eligible = catchAsync( async( req, res, next ) => {
     const userId = req.user;
     const user = await getUser({ _id: userId })
 
+    if( user.isAdmin ){
+        throw new AppError('Unauthorized action!', StatusCodes.UNAUTHORIZED )
+    }
+
     if ( !user.ticketAvailable ){
         throw new AppError("Check back tomorrow for tickets!", StatusCodes.BAD_REQUEST )
     }
