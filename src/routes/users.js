bd4d877fb0434416d5e8949224f2f1e427a73ccd
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-04-01 23:22:19 +0100

    improve: properly registered middlewares

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const userController = require('../controllers/users')
-
+const authenticate = require('../middlewares/authenticate')
 const protect = require('../middlewares/protect')
-const validateId = require('../middlewares/validate')
+const validateId = require('../middlewares/validateUserId')
 const userRouter = express.Router()
 
 userRouter
     .route('/')
-    .get( protect, userController.getEmployees )
+    .get( authenticate, protect(["admin"]), userController.getEmployees )
 
 userRouter
     .route('/:id')
-    .delete( protect, validateId, userController.deleteUser )
+    .delete( authenticate, protect(["admin"]), validateId, userController.deleteUser )
 
 module.exports={ userRouter }
