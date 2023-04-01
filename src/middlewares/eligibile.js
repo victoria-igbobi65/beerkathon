be6d0959151279c6d82ce77447b2c95819291c5b
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-04-01 23:16:53 +0100

    improve: modified to use user object instead of fetching user data from the db again

--- src/middlewares/eligibile.js
+++ src/middlewares/eligibile.js
@@ -6,12 +6,7 @@ const { getUser } = require('../services/auth');
 
 const eligible = catchAsync( async( req, res, next ) => {
     
-    const userId = req.user;
-    const user = await getUser({ _id: userId })
-
-    if( user.isAdmin ){
-        throw new AppError('Unauthorized action!', StatusCodes.UNAUTHORIZED )
-    }
+    const user = req.user;
 
     if ( !user.ticketAvailable ){
         throw new AppError("Check back tomorrow for tickets!", StatusCodes.BAD_REQUEST )
