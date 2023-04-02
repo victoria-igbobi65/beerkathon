8a93fc8463e10dea9a2953fe6d2c36082cdd06a2
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-04-02 18:17:16 +0100

    new: added date and query destructor helper

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,12 +1,12 @@
-
+const moment = require("moment");
 const jwt = require("jsonwebtoken");
-const { nanoid } = require('nanoid');
-const { promisify } = require('util')
-const CONFIG = require('../../config/env')
+const { nanoid } = require("nanoid");
+const { promisify } = require("util");
+const CONFIG = require("../../config/env");
 
 const uniqueId = () => {
-    return nanoid()
-}
+    return nanoid();
+};
 
 const signToken = (id) => {
     return jwt.sign({ id }, CONFIG.SECRET);
@@ -16,7 +16,7 @@ const setCookies = (res, tokenName, token) => {
     return res.cookie(tokenName, token, {
         httpOnly: true,
         secure: CONFIG.AP,
-        maxAge: CONFIG.MAXAGE
+        maxAge: CONFIG.MAXAGE,
     });
 };
 
@@ -26,7 +26,39 @@ const decodeToken = async (token) => {
 
 const generateTicket = () => {
     return nanoid(5, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+};
+
+const date = () => {
+    const startOfWeek = moment().startOf("week").toDate();
+    const endOfWeek = moment().endOf("week").toDate();
+    const today = moment().startOf('day').toDate()
+    const month = moment().startOf('month').toDate();
+
+    return { startOfWeek, endOfWeek, today, month };
+};
+
+const APIFEATURES = ( today, object ) => {
+
+    let query = today ? { createdAt: { $gte: date().today }} : {};
+    const sortBy = object.sort? object.sort.split(',').join(' '): '-createdAt';
+    const page = +object.page || 1;
+    const limit = +object.limit || 10;
+    const skip = (page - 1) * limit;
+
+    if ( object.status ){
+        query.status = object.status;
+    }
+
+    return { sortBy, query, limit, skip }
+
 }
 
-
-module.exports={ uniqueId, signToken, setCookies, decodeToken, generateTicket }
+module.exports = {
+    uniqueId,
+    signToken,
+    setCookies,
+    decodeToken,
+    generateTicket,
+    date,
+    APIFEATURES
+};
