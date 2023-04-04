2bdb3d3ad73ff245d32ba3c56d5e4aa9582bd725
Victoria Igbobi <victoriaigbobi65@gmail.com>
2023-04-04 22:29:58 +0100

    new: helper file for tests

---
+++ tests/test_helper.js
@@ -0,0 +1,21 @@
+exports.fakeLoginDetails = {
+    id: "Administhegreates@gmail.com",
+    password: "thegreatestHero"
+
+}
+
+exports.emtpyLoginDetails = {
+    id: "",
+    password: ""
+}
+
+
+exports.employeeEmailLoginDetails = {
+    id: "user@gmail.com",
+    password: "user12345",
+};
+
+exports.employeeIDLoginDetails = {
+    id: "00000",
+    password: "user12345",
+};
